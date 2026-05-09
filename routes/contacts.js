e090f3c4c04c1c5d73774047a68b15cbff3351a8
RebeccaHanks <han22023@byui.edu>
2026-05-09 15:59:31 -0600

    adding Post, Put, and Delete in contact.js and adding their route tests

--- routes/contacts.js
+++ routes/contacts.js
@@ -21,4 +21,71 @@ router.get('/:id', async (req, res) => {
   res.json(contact);
 });
 
+
+router.post('/', async(req,res) => {
+  const database = getDatabase();
+
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+   if (
+    !contact.firstName ||
+    !contact.lastName ||
+    !contact.email ||
+    !contact.favoriteColor ||
+    !contact.birthday
+   ) {
+    return res.status(400).json({ message: 'All feilds are required' });
+   }
+
+   const response = await database.collection('contacts').insertOne(contact);
+
+   res.status(201).json({ id: response.insertedId });
+   
+
+});
+
+router.put('/:id', async (req, res) => {
+  const database = getDatabase();
+  const contactId = new ObjectId(req.params.id);
+
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  if (
+    !contact.firstName ||
+    !contact.lastName ||
+    !contact.email ||
+    !contact.favoriteColor ||
+    !contact.birthday
+  ) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  await database.collection('contacts').replaceOne({ _id: contactId }, contact);
+
+  res.sendStatus(204);
+
+});
+
+router.delete('/:id', async (req, res) => {
+  const database = getDatabase();
+  const contactId = new ObjectId(req.params.id);
+
+  await database.collection('contacts').deleteOne({ _id: contactId });
+
+  res.sendStatus(200);
+
+});
+
 module.exports = router;
