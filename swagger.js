13b567980d037918a5cf453a96cd5c48ddf2d5bc
RebeccaHanks <han22023@byui.edu>
2026-05-16 11:27:15 -0600

    Update Swagger to use HTTPS

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Contacts API with MongoDB'
   },
   host: 'wdd-341-web-1.onrender.com',
-  schemes: ['http']
+  schemes: ['https']
 };
 
 const outputFile = './swagger-output.json';
